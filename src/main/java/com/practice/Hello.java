bd226430a8457402061344a1911048913e22d8ec
nandakishore1263 <nandakishore.g1263@gmail.com>
2026-06-26 13:19:10 +0530

    Create Hello.java

---
+++ src/main/java/com/practice/Hello.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Project 2!");
+    }
+
+    public static String greet(String name) {
+        return "Hello " + name + "!";
+    }
+}
